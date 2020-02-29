29995dc770f1e488b3437546c499d6e42af85336
Ayush412 <42313795+Ayush412@users.noreply.github.com>
2020-02-29 16:17:53 +0300

    Update reviews.dart

--- lib/reviews.dart
+++ lib/reviews.dart
@@ -99,42 +99,44 @@ class _reviewsState extends State<reviews> {
           : MediaQuery.removePadding(
                 context: context,
                 removeTop: true,
-                child: ListView.builder(
-                  controller: controller,
-                  itemCount: products.length,
-                  itemBuilder: (context, index) {
-                  return Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top: 15, bottom: 15, left: 5, right: 5),
-                        child: Stack(
-                          children: <Widget>[
-                            Icon(Icons.account_circle, color: Colors.white,),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 35),
-                              child: Text(products[index].data['Name'], style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500))
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top:35, left: 5),
-                              child: stars(products[index].data['Rate'].toDouble()),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 60),
-                              child: Text(products[index].data['Text'], style: TextStyle(color: Colors.white, fontSize: 17, fontWeight: FontWeight.w300, wordSpacing: 2, height: 1.3))
-                            ),
-                            Positioned(right: 5, top: 5,
-                            child: Text(products[index].data['Date'], style: TextStyle(color: Colors.grey),),
-                            )
-                          ],
+                child: Scrollbar(
+                  child: ListView.builder(
+                    controller: controller,
+                    itemCount: products.length,
+                    itemBuilder: (context, index) {
+                    return Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 15, bottom: 15, left: 5, right: 5),
+                          child: Stack(
+                            children: <Widget>[
+                              Icon(Icons.account_circle, color: Colors.white,),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 35),
+                                child: Text(products[index].data['Name'], style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w500))
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top:35, left: 5),
+                                child: stars(products[index].data['Rate'].toDouble()),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 60),
+                                child: Text(products[index].data['Text'], style: TextStyle(color: Colors.white, fontSize: 17, fontWeight: FontWeight.w300, wordSpacing: 2, height: 1.3))
+                              ),
+                              Positioned(right: 5, top: 5,
+                              child: Text(products[index].data['Date'], style: TextStyle(color: Colors.grey),),
+                              )
+                            ],
+                          ),
+                        ), 
+                        Divider(
+                          height: 0.2,
+                          color: Colors.grey,
                         ),
-                      ), 
-                      Divider(
-                        height: 0.2,
-                        color: Colors.grey,
-                      ),
-                    ],
-                  );
-                },
+                      ],
+                    );
+                  },
+                  ),
                 ),
               ),
       );
